99ef51df835dc65def62b3e9f9384f05ffacaee0
SAYAN GHOSH <97933977+SayanGhosh14112003@users.noreply.github.com>
2025-09-29 22:06:33 +0530

    Update product.controller.js

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -42,7 +42,7 @@ export const getFeaturedProducts = async (req, res) => {
 export const createProduct = async (req, res) => {
 	try {
 		const { name, description, price, image, category } = req.body;
-
+		console.log(image)
 		let cloudinaryResponse = null;
 
 		if (image) {
